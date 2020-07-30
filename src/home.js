48c52e1276c9e076ca170ddd489dbcee00cf27a2
hillarioh <okerioh@gmail.com>
2020-07-30 13:28:25 +0300

    add home page

---
+++ src/home.js
@@ -0,0 +1,10 @@
+import "../styles/style.css";
+
+const mainContent = document.createElement('div');
+mainContent.classList.add('mainContent');
+
+const mainTitle = document.createElement('p');
+
+
+
+export default mainContent;
