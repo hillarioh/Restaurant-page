4c6d827cc2cedee0b59200f2b8e06f82450ddb73
hillarioh <okerioh@gmail.com>
2020-07-31 09:00:22 +0300

    update foodlist

--- src/foodList.js
+++ src/foodList.js
@@ -1,4 +1,12 @@
-import icon2 from "../images/cheese-burger.jpg"
+import icon2 from "../images/cheese-burger.jpg";
+import beans from "../images/beans.jpg";
+import fish from "../images/fish.jpg";
+import mok from "../images/mok.jpg";
+import pilau from "../images/pilau.jpg";
+import potato from "../images/potato.jpg";
+import rosta from "../images/rosta.jpg";
+import samosa from "../images/samosa.jpg";
+import spice from "../images/spice.jpg";
 
 let menuObj = {
     fid1: {
@@ -7,25 +15,45 @@ let menuObj = {
         price: 5
     },
     fid2: {
-        name: "Beef steak",
-        image: icon2,
-            price: 45
+        name: "Beans with potato",
+        image: beans,
+            price: 5
     },
     fid3: {
-        name: "Roasted chicken",
-        image: icon2,
-            price: 55
+        name: "Fish ugali with traditional veges",
+        image: fish,
+            price: 50
     },
     fid4: {
-        name: "Oat meal",
-        image: icon2,
+        name: "Local smashed maizel",
+        image: mok,
             price: 10
     },
     fid5: {
-        name: "Morning Tea ",
-        image: icon2,
+        name: "Pilau special ",
+        image: pilau,
             price: 15
-    }
+    },
+    fid6: {
+        name: "Fried potatos mix",
+        image: potato,
+        price: 50
+    },
+    fid7: {
+        name: "Fried meat and skales",
+        image: rosta,
+        price: 40
+    },
+    fid8: {
+        name: "Chaposmocha",
+        image: samosa,
+        price: 30
+    },
+    fid9: {
+        name: "Fried chickenl",
+        image: spice,
+        price: 60
+    },
 }
 
 export default menuObj;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import header from "./nav";
 import home from './home';
 import menu from './menu';
+import contact from './contact';
 
 const content = document.getElementById("content");
 content.appendChild(header);
-
 // content.appendChild(home);
-content.appendChild(menu);
+// content.appendChild(menu);
+content.appendChild(contact);
 
