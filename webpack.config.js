bc6c98878c0a2d3ab2bb9b821466780010096e5f
hillarioh <okerioh@gmail.com>
2020-07-31 09:19:03 +0300

    add image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,35 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-           use: [
-              'file-loader',
-               ],
-             },
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
     ],
   },
 };
