5fb5b7872376cb971865e4eb29d7900d20bcc6c2
hillarioh <okerioh@gmail.com>
2020-07-31 09:13:06 +0300

    add switch statement for tab

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import header from "./nav";
 import home from './home';
 import menu from './menu';
 import contact from './contact';
+import tab from './control';
 
 const content = document.getElementById("content");
 content.appendChild(header);
-// content.appendChild(home);
+
+switch (tab) {
+    case 'home':
+        content.appendChild(home);
+        tab = 'home';
+        break;
+    case 'menu':
+        content.appendChild(menu);
+        tab = 'menu';
+        break;
+    case 'contact':
+        content.appendChild(contact);
+        tab = 'contact';
+        break;
+    default:
+        content.appendChild(home);
+}
+
 // content.appendChild(menu);
 content.appendChild(contact);
 
