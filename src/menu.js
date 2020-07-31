14d903e0a5de049fc1b591191e4914c47ee6cce2
hillarioh <okerioh@gmail.com>
2020-07-31 09:16:05 +0300

    add menu.js

---
+++ src/menu.js
@@ -0,0 +1,29 @@
+import "../styles/style.css";
+import menuObj from "./foodList";
+
+const foods = document.createElement('div');
+foods.setAttribute("id","foods");
+
+let items = '';
+for (let item in menuObj) {
+    const foodItem = document.createElement('div');
+    foodItem.setAttribute("class","item")
+    const foodImg = document.createElement('div');
+    foodImg.setAttribute("id", "img")
+    const foodName = document.createElement('h5');
+    const foodPrice = document.createElement('p');
+
+    foodImg.style.backgroundImage = `url('${menuObj[item].image}')`;
+    foodItem.appendChild(foodImg);
+    foodName.textContent = menuObj[item].name ;
+    foodItem.appendChild(foodName);
+    foodPrice.textContent = `Price: $${menuObj[item].price}`;
+    foodItem.appendChild(foodPrice);
+
+    foods.appendChild(foodItem);
+   
+}
+
+export default foods;
+
+
