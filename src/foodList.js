3ec9be9ed7508238b209ec8fa5a0ec216e2e621d
hillarioh <okerioh@gmail.com>
2020-07-31 09:16:29 +0300

    add foodlist

---
+++ src/foodList.js
@@ -0,0 +1,31 @@
+import icon2 from "../images/cheese-burger.jpg"
+
+let menuObj = {
+    fid1: {
+        name: "Hamburger ",
+        image: icon2,
+        price: 5
+    },
+    fid2: {
+        name: "Beef steak",
+        image: icon2,
+            price: 45
+    },
+    fid3: {
+        name: "Roasted chicken",
+        image: icon2,
+            price: 55
+    },
+    fid4: {
+        name: "Oat meal",
+        image: icon2,
+            price: 10
+    },
+    fid5: {
+        name: "Morning Tea ",
+        image: icon2,
+            price: 15
+    }
+}
+
+export default menuObj;
