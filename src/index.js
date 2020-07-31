5bc393ec58566ae34788551ae610f76171546f2c
hillarioh <okerioh@gmail.com>
2020-07-31 09:21:37 +0300

    edit home, index files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import header from "./nav";
 import home from './home';
+import menu from './menu';
 
 const content = document.getElementById("content");
 content.appendChild(header);
-content.appendChild(home);
+
+// content.appendChild(home);
+content.appendChild(menu);
 
